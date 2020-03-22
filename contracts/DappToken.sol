f37d77f40fa5c594fe4156854744b885bd1b3663
asusrid <32960104+asusrid@users.noreply.github.com>
2020-03-22 18:04:58 -0500

    Set up project
    Network and contructor created

---
+++ contracts/DappToken.sol
@@ -0,0 +1,20 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+
+
+contract DappToken {
+
+	// Constructor (set the number of tokens we need)
+	// Set the total number of tokens
+	// Read the total number of tokens
+	uint256 public totalSupply;
+	
+	function DappToken() public {
+		totalSupply = 1000000;
+	}
+
+
+
+
+
+} 
