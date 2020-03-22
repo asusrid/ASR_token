eeba447ce88d6200dc152bf6ea82c92c42114e7a
asusrid <32960104+asusrid@users.noreply.github.com>
2020-03-22 18:22:50 -0500

    First deploy of contracts

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,8 +8,8 @@ contract DappToken {
 	// Set the total number of tokens
 	// Read the total number of tokens
 	uint256 public totalSupply;
-	
-	function DappToken() public {
+
+	constructor () public {
 		totalSupply = 1000000;
 	}
 
