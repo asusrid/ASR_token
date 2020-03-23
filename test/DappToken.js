b532e6cf82de23d3e7d5833ba2376742ec81e628
asusrid <32960104+asusrid@users.noreply.github.com>
2020-03-23 13:03:45 -0500

    Balance of owner implemented! #7

--- test/DappToken.js
+++ test/DappToken.js
@@ -2,12 +2,17 @@ const DappToken = artifacts.require("DappToken");
 
 contract('DappToken', function(accounts){
 
+	var tokenInstance;
+
 	it('sets the total sumply upon deployment', function(){
 		return DappToken.deployed().then(function(instance){
 			tokenInstance = instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply){
 			assert.equal(totalSupply.toNumber(), 1000000, 'the total number supply must be 1M');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(adminBalance){
+			assert.equal(adminBalance.toNumber(), 1000000, 'the total number supply must be 1M');
 		});
 	});
 })
